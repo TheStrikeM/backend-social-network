fabfa5a26fc2c13e40d65b424049ef25f059e315
Danil <dinkodanil@gmail.com>
2021-03-16 12:52:29 +0300

    Create UserDto

--- src/modules/user/dto/UserDto.ts
+++ src/modules/user/dto/UserDto.ts
@@ -1,6 +1,33 @@
+import { ObjectId } from 'mongoose';
+
 export enum Role {
   USER = 'USER',
   ADMIN = 'ADMIN',
   MANAGER = 'MANAGER',
   MEDIA = 'MEDIA',
 }
+
+export type UserDto = {
+  readonly _id?: number;
+  readonly email?: string;
+  readonly full_name?: string;
+  readonly desc?: string;
+  readonly password?: string;
+  readonly avatar?: string;
+  readonly photos?: string[];
+  readonly country?: string;
+  readonly state?: string;
+  readonly registredIn?: Date;
+  readonly verify?: boolean;
+  readonly confirmed?: boolean;
+  readonly online?: boolean;
+  readonly role?: Role;
+  readonly website?: string;
+  readonly friends?: ObjectId[];
+  readonly subscribers?: ObjectId[];
+  readonly subscriptions?: ObjectId[];
+  readonly posts?: string[];
+  readonly reposts?: string[];
+  readonly comments?: string[];
+  readonly postComments?: string[];
+};
