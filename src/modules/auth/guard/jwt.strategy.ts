eed258aa5a2c3f421c1373dbcf66684ab7fc945c
Danil <dinkodanil@gmail.com>
2021-03-16 13:15:03 +0300

    Create jwt strategy

--- src/modules/auth/guard/jwt.strategy.ts
+++ src/modules/auth/guard/jwt.strategy.ts
@@ -1,7 +1,19 @@
-import {Injectable} from "@nestjs/common";
-
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export default class JwtStrategy extends {
+export default class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET_KEY'),
+    });
+  }
 
+  async validate(payload: any) {
+    return payload.user;
+  }
 }
