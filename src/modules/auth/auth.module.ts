57cf9ce0530c51294527ec68e39ec12074337d5b
Danil <dinkodanil@gmail.com>
2021-03-16 13:08:57 +0300

    connect userrepo

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import UserRepository from '../user/service/user.repository';
 
 @Module({
   imports: [],
   providers: [],
   controllers: [],
+  exports: [UserRepository],
 })
 export default class AuthModule {}
