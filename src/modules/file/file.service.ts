4fe49a6bc1c7c215d5800d6f76ea4d27a52493cc
Danil <dinkodanil@gmail.com>
2021-03-16 15:48:10 +0300

    createfile

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -1,11 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import * as uuid from 'uuid';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export default class FileService {
-  constructor() {}
-
   createFile(file): string {
-    const fileExtension = file.originalname.split('.').pop;
-    return 'Привет';
+    try {
+      const fileExtension = file.originalname.split('.').pop;
+      const fileName = `${uuid.v4()}.${fileExtension()}`;
+      const filePath = path.resolve(__dirname, '..', '..', 'static', 'img');
+
+      if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath, { recursive: true });
+      }
+      return fileName;
+    } catch (e) {
+      throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
