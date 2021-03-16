f16e45307d274ff1d4c493616dd083a19877ea2f
Danil <dinkodanil@gmail.com>
2021-03-16 13:38:56 +0300

    npm i @nestjs/jwt and setting auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import UserRepository from '../user/service/user.repository';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import JwtStrategy from './strategy/jwt.strategy';
+import LocalStrategy from './strategy/local.strategy';
+import CryptoService from './service/crypto.service';
+import AuthService from './service/auth.service';
 
 @Module({
-  imports: [],
-  providers: [],
+  imports: [
+    PassportModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [JwtStrategy, LocalStrategy, CryptoService, AuthService],
   controllers: [],
   exports: [UserRepository],
 })
