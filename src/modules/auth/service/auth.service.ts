2f179aaffb1322fd82713564666cf8f43d68a965
Danil <dinkodanil@gmail.com>
2021-03-16 13:47:34 +0300

    create generateToken and register function

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import UserRepository from '../../user/service/user.repository';
 import CryptoService from './crypto.service';
-import { UserDto, UserLoginDto } from '../../user/dto/UserDto';
+import { UserDto, UserLoginDto, UserRegisterDto } from '../../user/dto/UserDto';
 import { User } from '../../user/schema/User';
+import { JwtService } from '@nestjs/jwt';
 
 export type SecureUser = Pick<
   User,
@@ -29,11 +30,18 @@ export type SecureUser = Pick<
   | 'postComments'
 >;
 
+export type GeneratedToken<U> = {
+  message: string;
+  accessToken: string;
+  user: U;
+};
+
 @Injectable()
 export default class AuthService {
   constructor(
     private readonly userRepository: UserRepository,
     private readonly cryptoService: CryptoService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async validateUser(dto: UserLoginDto): Promise<SecureUser | null> {
@@ -47,5 +55,27 @@ export default class AuthService {
     }
 
     return null;
+  }
+
+  async generateToken(user): Promise<GeneratedToken<any>> {
+    const { password, ...payload } = user._doc;
+    const token = await this.jwtService.signAsync({ user: payload });
+    return {
+      message: 'Вы успешно авторизовались!',
+      accessToken: token,
+      user: payload,
+    };
+  }
+
+  async register(user: UserRegisterDto): Promise<User | void> {
+    const candidate: User = await this.userRepository.findByUsername(
+      user.username,
+    );
+    if (candidate) {
+      throw new Error('Данный пользователь уже существует');
+    }
+
+    const encryptedPassword = this.cryptoService.encrypt(user.password);
+    return this.userRepository.create({ ...user, password: encryptedPassword });
   }
 }
