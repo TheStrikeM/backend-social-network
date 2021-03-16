ffb30978f5503eaaf94b9c1a15787eed692b5ca8
Danil <dinkodanil@gmail.com>
2021-03-16 15:57:27 +0300

    deletefile

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -5,16 +5,45 @@ import * as fs from 'fs';
 
 @Injectable()
 export default class FileService {
-  createFile(file): string {
+  createFile(username, fileType, file): string {
     try {
       const fileExtension = file.originalname.split('.').pop;
       const fileName = `${uuid.v4()}.${fileExtension()}`;
-      const filePath = path.resolve(__dirname, '..', '..', 'static', 'img');
+      const filePath = path.resolve(
+        __dirname,
+        '..',
+        '..',
+        'static',
+        username,
+        fileType,
+      );
+      console.log(filePath);
 
       if (!fs.existsSync(filePath)) {
         fs.mkdirSync(filePath, { recursive: true });
       }
-      return fileName;
+      return `${username}/${fileType}/${fileName}`;
+    } catch (e) {
+      throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  deleteFile(username, fileType, fileName): string {
+    try {
+      const filePath = path.resolve(
+        __dirname,
+        '..',
+        '..',
+        'static',
+        username,
+        fileType,
+        fileName,
+      );
+
+      if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+      }
+      return `${username}/${fileType}/${fileName}`;
     } catch (e) {
       throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
