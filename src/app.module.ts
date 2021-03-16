c3961563287b17f24367ba42e8316540d5f93095
Danil <dinkodanil@gmail.com>
2021-03-16 16:05:27 +0300

    start developing profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import UserModule from './modules/user/user.module';
 import AuthModule from './modules/auth/auth.module';
+import FileModule from './modules/file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 const dbURI =
   'mongodb+srv://admin:admin@cluster0.ydv37.mongodb.net/social-network?retryWrites=true&w=majority';
@@ -11,8 +14,10 @@ const dbURI =
   imports: [
     MongooseModule.forRoot(dbURI),
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
     UserModule,
     AuthModule,
+    FileModule,
   ],
 })
 export class AppModule {}
