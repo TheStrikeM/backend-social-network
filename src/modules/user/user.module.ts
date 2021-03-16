5b78bb12a1a8ae0bdbd857e20234b16785a78ecb
Danil <dinkodanil@gmail.com>
2021-03-16 12:54:30 +0300

    Connect schema to module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schema/User';
+import UserRepository from './service/user.repository';
 
 @Module({
-  imports: [],
-  providers: [],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UserRepository],
   controllers: [],
 })
 export default class UserModule {}
