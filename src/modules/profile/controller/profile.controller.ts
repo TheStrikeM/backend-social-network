bcf3a7374573243851fc0bbd89f0e377eb3b65b6
Danil <dinkodanil@gmail.com>
2021-03-17 15:18:20 +0300

    Refactor profile controller and setting the profile module (add refactored services and controllers)

---
+++ src/modules/profile/controller/profile.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
+import JwtGuard from '../../auth/guard/jwt.guard';
+import ProfileService from '../service/profile.service';
+
+@Controller('profile')
+export default class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Get('my')
+  @UseGuards(JwtGuard)
+  async myProfile(@Request() req) {
+    return this.profileService.myProfile(req.user._id);
+  }
+
+  @Get(':username')
+  async getProfile(@Param('username') username: string) {
+    return this.profileService.getProfile(username);
+  }
+}
--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import ProfileService from './service/profile.service';
-import ProfileController from './profile.controller';
+import ProfileController from './controller/profile.controller';
 import FileModule from '../file/file.module';
 import UserModule from '../user/user.module';
 import AuthModule from '../auth/auth.module';
 import SubscribeService from './service/subscribe.service';
 import PhotosService from './service/photos.service';
+import SubscribeController from './controller/subscribe.controller';
 
 @Module({
   imports: [FileModule, UserModule, AuthModule],
   providers: [ProfileService, SubscribeService, PhotosService],
-  controllers: [ProfileController],
+  controllers: [ProfileController, SubscribeController],
 })
 export default class ProfileModule {}
