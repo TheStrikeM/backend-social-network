e8c543f6fcd8cbca832e5bc916385f8a837432af
Danil <dinkodanil@gmail.com>
2021-03-18 13:28:39 +0300

    Add schema and dto to Post module

---
+++ src/modules/post/dto/PostDto.ts
@@ -0,0 +1,33 @@
+import { ObjectId } from 'mongoose';
+import { MaxLength, MinLength } from 'class-validator';
+
+export class PostVerifiedDto {
+  @MinLength(5, {
+    message: 'Ваш юзернейм должен состоять из не менее 5 символов',
+  })
+  @MaxLength(35, {
+    message: 'Ваш юзернейм должен состоять из не менее 35 символов',
+  })
+  title: string;
+
+  photo?: string;
+
+  @MinLength(5, {
+    message: 'Ваш юзернейм должен состоять из не менее 5 символов',
+  })
+  @MaxLength(35, {
+    message: 'Ваш юзернейм должен состоять из не менее 35 символов',
+  })
+  desc: string;
+}
+
+export type PostDto = {
+  readonly _id: ObjectId;
+  readonly authorId: ObjectId[];
+  readonly title: string;
+  readonly photo: string;
+  readonly desc: string;
+  readonly createdId: Date;
+  readonly comments: ObjectId[];
+  readonly reposts: ObjectId[];
+};
---
+++ src/modules/post/schema/Post.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { User } from '../../user/schema/User';
+
+export type PostDocument = PopStateEvent & Document;
+
+@Schema()
+export class Post {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  authorId: User;
+
+  @Prop({ type: String, min: 5, max: 25, required: true })
+  title: string;
+
+  @Prop({ type: String, min: 5, max: 25, required: true })
+  desc: string;
+
+  @Prop({ type: String, default: '' })
+  photo: string;
+
+  @Prop({ type: Date, required: true })
+  createdIn: Date;
+
+  @Prop()
+  comments: string[];
+
+  @Prop()
+  reposts: string[];
+}
+
+export const PopStateEventSchema = SchemaFactory.createForClass(Post);
